c6d119dbcc0150fd65588141607abe89fb418bef
Beatrice Washington <bea800.w98@gmail.com>
2023-05-08 20:55:03 +0400

    Routing

--- App/client/src/App.js
+++ App/client/src/App.js
@@ -1,12 +1,52 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//import all components
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
+
+//Root Routes 
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Username></Username>
+  },
+  {
+    path : '/register',
+    element : <Register></Register>
+  },
+  {
+    path : '/password',
+    element : <Password></Password>
+  },
+  {
+    path : '/profile',
+    element : <Profile></Profile>
+  },
+  {
+    path : '/recovery',
+    element : <Recovery></Recovery>
+  },
+  {
+    path : '/reset',
+    element : <Reset></Reset>
+  },
+  {
+    path : '/PageNotFound',
+    element : <PageNotFound></PageNotFound>
+  },
+])
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello World !
-      </h1>
-    </div>
+    <main>
+     <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 };
 
