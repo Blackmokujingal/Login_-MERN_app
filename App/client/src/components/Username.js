1aabd9033ce1a169aaf4bcbadcc71c69460194cc
Beatrice Washington <bea800.w98@gmail.com>
2023-05-11 12:47:26 +0400

    Username handling using Toast + Formik

--- App/client/src/components/Username.js
+++ App/client/src/components/Username.js
@@ -1,27 +1,49 @@
 import React from "react";
 import avatar from "../assets/profile.png";
 import { Link } from "react-router-dom";
+import { Toaster } from 'react-hot-toast'
+import { useFormik} from 'formik'
+import { usernameValidate } from "../helper/validate";
 
 import styles from "../styles/Username.module.css";
 
 
 const Username = () => {
+
+  const formik = useFormik({
+    initialValues : {
+      username : ''
+    },
+    validate :  usernameValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit : async values => {
+      console.log(values)
+    }
+  })
+
+
+
   return (
     <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder
+      ='false'>
+
+      </Toaster>
       <div className="flex justify-center items-center h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
             <h4 className="text-5xl font-bold text-indigo-500">Hello Again !</h4>
             <span className="py-4 text-xl w-2/3 text-center text-gray-500">
-              Explore More by connecting with us{" "}
+              Explore more by connecting with us{" "}
             </span>
           </div>
-          <form className="py-1">
+          <form className="py-1" onSubmit={formik.handleSubmit}>
             <div className="profile flex justify-center py-4">
               <img src={avatar} className={styles.profile_img} alt="avatar" />
             </div>
             <div className="textbox flex flex-col items-center gap-6">
-              <input className={styles.textbox} type="text" placeholder="Username" />
+              <input {...formik.getFieldProps('username')} className={styles.textbox} type="text" placeholder="Username" />
               <button className={styles.btn} type="submit">Let's Go </button>
             </div>
             <div className="text-center py-4">
