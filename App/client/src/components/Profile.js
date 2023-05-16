9ac4a0b905dc20b7295fc01a2704855b5b2b71c3
Beatrice Washington <bea800.w98@gmail.com>
2023-05-16 16:35:48 +0400

    Profile UI

--- App/client/src/components/Profile.js
+++ App/client/src/components/Profile.js
@@ -1,11 +1,128 @@
-import React from 'react'
+import React, { useState } from "react";
+import avatar from "../assets/profile.png";
+import { Link } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import { passwordValidate } from "../helper/validate";
+import convertToBase64 from "../helper/convert";
 
-const Profile = () => {
+import styles from "../styles/Username.module.css";
+import extend from "../styles/Profile.module.css";
+
+const Register = () => {
+  const [file, setFile] = useState();
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      mobile: "",
+      email: "",
+      address: "",
+    },
+    validate: passwordValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      values = await Object.assign(values, { profile: file });
+      console.log(values);
+    },
+  });
+
+  // formik doensn't support file upload so we need to create this handler
+
+  const onUpload = async (e) => {
+    const base64 = await convertToBase64(e.target.files[0]);
+    setFile(base64);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder="false"></Toaster>
+      <div className="flex justify-center items-center h-screen">
+        <div
+          className={`${styles.glass} ${extend.glass}`}
+          style={{ width: "45%", paddingTop: "3em" }}
+        >
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold text-indigo-500">
+              Build your profile{" "}
+            </h4>
+            <span className="py-3 text-xl w-2/3 text-center text-gray-500">
+              Tell us more about you{" "}
+            </span>
+          </div>
+          <form className="py-1" onSubmit={formik.handleSubmit}>
+            <div className="profile flex justify-center py-3">
+              <label htmlFor="profile">
+                <img
+                  src={file || avatar}
+                  className={`${styles.profile_img} ${extend.profile_img}`}
+                  alt="avatar"
+                />
+              </label>
+              <input
+                onChange={onUpload}
+                type="file"
+                id="profile"
+                name="profile"
+              />
+            </div>
 
-export default Profile
+            <div className="textbox flex flex-col items-center gap-4">
+              <div className="name flex w-3/4 gap-10">
+                <input
+                  {...formik.getFieldProps("firstName")}
+                  className={`${styles.textbox} ${extend.textbox}`}
+                  type="text"
+                  placeholder="First name*"
+                />
+                <input
+                  {...formik.getFieldProps("lastName")}
+                  className={`${styles.textbox} ${extend.textbox}`}
+                  type="text"
+                  placeholder="Last name*"
+                />
+              </div>
+
+              <div className="name flex w-3/4 gap-10">
+                <input
+                  {...formik.getFieldProps("mobile")}
+                  className={`${styles.textbox} ${extend.textbox}`}
+                  type="text"
+                  placeholder="Mobile number *"
+                />
+                <input
+                  {...formik.getFieldProps("email")}
+                  className={`${styles.textbox} ${extend.textbox}`}
+                  type="text"
+                  placeholder="Email*"
+                />
+              </div>
+
+              <input
+                {...formik.getFieldProps("address")}
+                className={styles.textbox}
+                type="text"
+                placeholder="Address *"
+              />
+              <button className={styles.btn} type="submit">
+                Register{" "}
+              </button>
+            </div>
+            <div className="text-center py-2">
+              <span className="text-gray-500">
+                Come back later ?{" "}
+                <Link className="text-red-500 p-l 2" to="/">
+                  Logout
+                </Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
