cdc76207962b39644e921bebdb378db31f8d5c1d
Beatrice Washington <bea800.w98@gmail.com>
2023-05-21 18:22:55 +0400

    GenerateOTP/ Verify TOP controller / API test

--- App/server/controllers/appController.js
+++ App/server/controllers/appController.js
@@ -179,13 +179,20 @@ export async function updateUser(req,res){
 }
 
 /** GET: http://localhost:8000/api/generateOTP */
-export async function generateOTP(req, res) {
-  res.json("generateOTP route");
+export async function generateOTP(req,res){
+  req.app.locals.OTP = await otpGenerator.generate(6, { lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false})
+  res.status(201).send({ code: req.app.locals.OTP })
 }
 
 /** GET: http://localhost:8000/api/verifyOTP */
-export async function verifyOTP(req, res) {
-  res.json("verifyOTP route");
+export async function verifyOTP(req,res){
+  const { code } = req.query;
+  if(parseInt(req.app.locals.OTP) === parseInt(code)){
+      req.app.locals.OTP = null; // reset the OTP value
+      req.app.locals.resetSession = true; // start session for reset password
+      return res.status(201).send({ msg: 'Verified Successsfully !'})
+  }
+  return res.status(400).send({ error: "Invalid OTP"});
 }
 
 /** GET: http://localhost:8000/api/createResetSession */
