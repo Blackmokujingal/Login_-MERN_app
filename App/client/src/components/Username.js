3c76b90f9102ea0fdaf0fa50cbd3f83e9ac8d807
Beatrice Washington <bea800.w98@gmail.com>
2023-05-23 14:35:47 +0400

    Zustand store

--- App/client/src/components/Username.js
+++ App/client/src/components/Username.js
@@ -1,39 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import avatar from "../assets/profile.png";
-import { Link } from "react-router-dom";
-import { Toaster } from 'react-hot-toast'
-import { useFormik} from 'formik'
+import { Link, useNavigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
 import { usernameValidate } from "../helper/validate";
+import { useAuthStore } from "../store/store";
 
 import styles from "../styles/Username.module.css";
 
-
 const Username = () => {
-
+  const navigate = useNavigate();
+  const setUsername = useAuthStore((state) => state.setUsername);
   const formik = useFormik({
-    initialValues : {
-      username : ''
+    initialValues: {
+      username: "example123",
     },
-    validate :  usernameValidate,
+    validate: usernameValidate,
     validateOnBlur: false,
     validateOnChange: false,
-    onSubmit : async values => {
-      console.log(values)
-    }
-  })
-
-
+    onSubmit: async (values) => {
+      setUsername(values.username);
+      navigate('/password')  
+    },
+  });
 
   return (
     <div className="container mx-auto">
-      <Toaster position="top-center" reverseOrder
-      ='false'>
-
-      </Toaster>
+      <Toaster position="top-center" reverseOrder="false"></Toaster>
       <div className="flex justify-center items-center h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
-            <h4 className="text-5xl font-bold text-indigo-500">Hello Again !</h4>
+            <h4 className="text-5xl font-bold text-indigo-500">
+              Hello Again !
+            </h4>
             <span className="py-4 text-xl w-2/3 text-center text-gray-500">
               Explore more by connecting with us{" "}
             </span>
@@ -43,8 +42,15 @@ const Username = () => {
               <img src={avatar} className={styles.profile_img} alt="avatar" />
             </div>
             <div className="textbox flex flex-col items-center gap-6">
-              <input {...formik.getFieldProps('username')} className={styles.textbox} type="text" placeholder="Username" />
-              <button className={styles.btn} type="submit">Let's Go </button>
+              <input
+                {...formik.getFieldProps("username")}
+                className={styles.textbox}
+                type="text"
+                placeholder="Username"
+              />
+              <button className={styles.btn} type="submit">
+                Let's Go{" "}
+              </button>
             </div>
             <div className="text-center py-4">
               <span className="text-gray-500">
--- App/client/src/helper/validate.js
+++ App/client/src/helper/validate.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-useless-escape */
 import toast from "react-hot-toast";
+import { authenticate } from './helper'
 
 // validate password
-
 function passwordVerify(errors = {}, values) {
   // eslint-disable no-useless-escape
   const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
@@ -41,8 +41,19 @@ export async function profileValidation(values){
 }
 
 // validate login page username
-export async function usernameValidate(values) {
+
+export async function usernameValidate(values){
   const errors = usernameVerify({}, values);
+
+  if(values.username){
+      // check user exist or not
+      const { status } = await authenticate(values.username);
+      
+      if(status !== 200){
+          errors.exist = toast.error('User does not exist !')
+      }
+  }
+
   return errors;
 }
 
